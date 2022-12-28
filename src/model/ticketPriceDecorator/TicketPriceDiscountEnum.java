5e79619b55ff06689ab0fc3631ad0c1f822782ec
Wannes-Schepers <wannes.schepers@student.ucll.be>
2022-12-28 18:35:03 +0100

    Story 5: UI + possible to add rides to a Card

--- src/model/ticketPriceDecorator/TicketPriceDiscountEnum.java
+++ src/model/ticketPriceDecorator/TicketPriceDiscountEnum.java
@@ -2,8 +2,16 @@ package model.ticketPriceDecorator;
 
 public enum TicketPriceDiscountEnum {
     //All possible discounts
-    AGE_64_PLUS_DISCOUNT,
-    CHRISTMAS_LEAVE_DISCOUNT,
-    STUDENT_DISCOUNT,
-    FREQUENT_TRAVELER_DISCOUNT,
+    AGE_64_PLUS_DISCOUNT("Age 64+ Discount", 0.15),
+    CHRISTMAS_LEAVE_DISCOUNT("Christmas Leave Discount", 0.10),
+    STUDENT_DISCOUNT("Student Discount", 0.25),
+    FREQUENT_TRAVELER_DISCOUNT("Frequent Traveler Discount", 0.20);
+
+    private String name;
+    private double discount;
+
+    TicketPriceDiscountEnum(String name, double discount) {
+        this.name = name;
+        this.discount = discount;
+    }
 }
